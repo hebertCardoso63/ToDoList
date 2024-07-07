2a95deb683372f3761bd5537ad2c784fa7c52fc6
hebertCardoso63 <cardosohebert34|@gmail.com>
2024-07-07 09:47:42 -0300

    # - Configuração inicial do projeto

---
+++ databases/knex/knexfile.ts
@@ -0,0 +1,37 @@
+import type { Knex } from "knex";
+import * as dotenv from 'dotenv';
+import { resolve } from 'path';
+dotenv.config();
+
+const {
+    DB_HOST,
+    DB_USER,
+    DB_PASS,
+    DB_NAME,
+} = process.env;
+
+console.log(88, DB_HOST, DB_USER, DB_PASS, DB_NAME)
+
+const config: { [key: string]: Knex.Config } = {
+    development: {
+        client: 'pg',
+        pool: {
+            min: 0,
+            max: 5,
+            idleTimeoutMillis: 10000,
+        },
+        connection: {
+            host: '127.0.0.1',
+            port: 5434,
+            user: 'root',
+            password: '123456',
+            database: 'toDoList',
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+            directory: resolve(__dirname, 'migrations'),
+        },
+    }
+};
+
+export default config;
