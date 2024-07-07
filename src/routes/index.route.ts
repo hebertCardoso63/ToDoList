af7a682b3bab5076ccf562fa652c3b19ffa40ca7
hebertCardoso63 <cardosohebert34|@gmail.com>
2024-07-07 15:08:52 -0300

    # - Mudanças no arquivo principal de rotas

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 import obterTodoList from './toDoList.route';
+import { authenticate } from '../middlewares/auth.middleware';
+import auth from './auth.route';
 
 const router = Router();
 
 router
-    .use('', obterTodoList);
+    .use(
+        '/auth',
+        auth,
+    )
+    .use(
+        '/api',
+        authenticate,
+        obterTodoList,
+    );
 
 export default router;
