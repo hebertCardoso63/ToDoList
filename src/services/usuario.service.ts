ad7968f4188db6a0af95472f649bed232f65e670
hebertCardoso63 <cardosohebert34|@gmail.com>
2024-07-07 15:10:26 -0300

    # - service de autenticação

---
+++ src/services/usuario.service.ts
@@ -0,0 +1,41 @@
+import knex from "../factories/knex.factory";
+import { DadosCadastrais, Usuario as UsuarioModel } from "../models/usuario.model";
+
+class Usuario {
+    private async verificarExistencia(nomeUsuario: string): Promise<boolean> {
+        const usuario = await this.encontrarUsuario(nomeUsuario)
+        
+        if (!usuario) {
+            return false;
+        }
+
+        return true;
+    }
+    public async encontrarUsuario(nomeUsuario: string): Promise<UsuarioModel | false> {
+        const usuario: UsuarioModel = await knex('usuarios')
+            .select(['*'])
+            .where({ nome_usuario: nomeUsuario })
+            .first();
+
+        if (!usuario) {
+            return false;
+        }
+
+        return usuario;
+    }
+
+    public async cadastrar(dadosCadastrais: DadosCadastrais): Promise<number | false>{
+        const existe = await this.verificarExistencia(dadosCadastrais.nome_usuario);
+        console.log(666, existe);
+        if (existe) {
+            return false;
+        }
+
+        const [ idUsuario ] = await knex('usuarios')
+            .insert(dadosCadastrais, ['id']);
+
+        return idUsuario.id;
+    }
+}
+
+export const usuario = new Usuario();
