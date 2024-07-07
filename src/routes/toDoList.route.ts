f2ae890c8e0c7e7581d60bf345565c16185ffef5
hebertCardoso63 <cardosohebert34|@gmail.com>
2024-07-07 15:09:43 -0300

    # - Apatacoes

--- src/routes/toDoList.route.ts
+++ src/routes/toDoList.route.ts
@@ -3,6 +3,6 @@ import { toDoList } from '../controllers/toDoList.controller';
 
 const router = Router();
 
-router.get('/api/lista-tarefas', toDoList.listaTarefas);
+router.get('/lista-tarefas', toDoList.listaTarefas);
 
 export default router;
