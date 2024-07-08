4d732a490e8aa4504ae27b7c286389aa309e30d2
hebertCardoso63 <cardosohebert34|@gmail.com>
2024-07-07 21:13:07 -0300

    # - service da rota

--- src/services/toDo.service.ts
+++ src/services/toDo.service.ts
@@ -1,11 +1,14 @@
+import knex from "../factories/knex.factory";
+import { ToDo as ToDoModel } from "../models/todo.model";
 
 class ToDo {
-    public async obterListaTarefas(): Promise<any>{
-        try {
-            console.log(999);
-        } catch (e) {
-            throw new Error('Falha ao listar dispositivos');
-        }
+    public async obterListaTarefas(usuarioId: number): Promise<ToDoModel[]> {
+        const listaAfazeres = await knex({ t: 'todos' })
+            .select(['*'])
+            .where('t.usuario_id', usuarioId)
+            .orderBy('t.id', 'desc');
+
+        return listaAfazeres;
     }
 }
 
