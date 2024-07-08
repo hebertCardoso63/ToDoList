f40ba9762d32b70a34460c6b8e32728dff8f73df
hebertCardoso63 <cardosohebert34|@gmail.com>
2024-07-07 23:33:27 -0300

    # - Controller Cadastro ToDo

--- src/controllers/toDoList.controller.ts
+++ src/controllers/toDoList.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import { toDo as toDoService } from "../services/toDo.service";
-import { ToDo } from "../models/todo.model";
+import { ToDo, CriarToDo } from "../models/todo.model";
 
 class ToDoList {
     constructor() {
         this.listaTarefas = this.listaTarefas.bind(this);
+        this.cadastrarToDo = this.cadastrarToDo.bind(this);
     }
 
     public async listaTarefas(req: Request, res: Response, next: NextFunction) {
@@ -16,6 +17,27 @@ class ToDoList {
             const listaTarefas: ToDo[] = await toDoService.obterListaTarefas(usuario.id);
 
             return res.status(200).json(listaTarefas);
+        } catch (err) {
+            next(err);
+        }
+    }
+
+    public async cadastrarToDo(req: Request, res: Response, next: NextFunction) {
+        if (!req.usuario) return res.status(401).json({ message: "Unauthorized" });
+
+        const usuario = req.usuario;
+
+        const dadosToDo: CriarToDo = {
+            titulo: req.body.titulo,
+            descricao: req.body.descricao,
+            status: req.body.status,
+            usuario_id: usuario.id,
+        };
+
+        try {
+            const idRegistro = await toDoService.criarToDo(dadosToDo);
+
+            return res.status(201).json({ message: 'ToDo criado com sucesso.', id_registro: idRegistro });
         } catch (err) {
             next(err);
         }
