73421c0d037f2a3ff6efa7a7b0ff416d80be7726
hebertCardoso63 <cardosohebert34|@gmail.com>
2024-07-07 21:12:41 -0300

    # - Endpoint da rota

--- src/routes/toDoList.route.ts
+++ src/routes/toDoList.route.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import { toDoList } from '../controllers/toDoList.controller';
+import { authenticate } from '../middlewares/auth.middleware';
 
 const router = Router();
 
-router.get('/lista-tarefas', toDoList.listaTarefas);
+router
+    .get(
+        '/lista-tarefas',
+        authenticate,
+        toDoList.listaTarefas
+    );
 
 export default router;
