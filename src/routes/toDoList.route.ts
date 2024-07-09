8716ca65b33042f64d86b7d297d3c5745881886a
hebertCardoso63 <cardosohebert34|@gmail.com>
2024-07-09 02:58:17 -0300

    # - Organizando routes todo

--- src/routes/toDoList.route.ts
+++ src/routes/toDoList.route.ts
@@ -7,22 +7,18 @@ const router = Router();
 router
     .get(
         '/todos/:todo_id',
-        authenticate,
         toDoList.buscarTodo
     )
     .get(
         '/lista-todo',
-        authenticate,
         toDoList.listarToDos
     )
     .post(
         '/cadastrar-todo',
-        authenticate,
         toDoList.cadastrarToDo
     )
     .patch(
         '/todos/:todo_id',
-        authenticate,
         toDoList.editarToDo,
     );
 
