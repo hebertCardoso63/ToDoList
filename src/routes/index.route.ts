ceaa648f4d2b88a1c7d2bd8433f133fc01b39b64
hebertCardoso63 <cardosohebert34|@gmail.com>
2024-07-09 02:57:30 -0300

    # - Organizando arquivo principal routes

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,19 +1,14 @@
 import { Router } from 'express';
 import obterTodoList from './toDoList.route';
 import { authenticate } from '../middlewares/auth.middleware';
+import { validateAuth } from '../validators/auth.validator';
+
 import auth from './auth.route';
 
 const router = Router();
 
 router
-    .use(
-        '/auth',
-        auth,
-    )
-    .use(
-        '/api',
-        authenticate,
-        obterTodoList,
-    );
+    .use('/auth', validateAuth, auth)
+    .use('/api', authenticate, obterTodoList);
 
 export default router;
